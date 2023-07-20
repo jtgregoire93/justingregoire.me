1e35a99aaca2c869da8ac2562bf10c75ff2812a5
Justin Gregoire <jtgregoire93@gmail.com>
2023-07-20 16:37:13 -0700

    fix download path

--- resume_gen/render.R
+++ resume_gen/render.R
@@ -7,7 +7,7 @@ library(pdftools)
 # declare param inputs to render
 sheet_ss_id <- "1SqkIDpyVfurLA0DXRgcsZ9aba_Bk9bvak0sGRCLsVX0"
 online_link <- "https://justingregoire.me/resume.html"
-pdf_location <- "https://github.com/jtgregoire93/justingregoire.me/raw/main/jgregoire_resume.pdf"
+pdf_location <- "https://github.com/jtgregoire93/justingregoire.me/raw/main/pdf/jgregoire_resume.pdf"
 # Knit the HTML version
 rmarkdown::render("resume_gen/resume_generation.Rmd", params = list(pdf_export = FALSE,
   sheet_ss_id = sheet_ss_id, online_link = online_link, pdf_location = pdf_location),
